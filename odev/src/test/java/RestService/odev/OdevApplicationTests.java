b1f1795deb2f759957e66363d1fe51edd6590be2
02230224093 <149946169+02230224093@users.noreply.github.com>
2024-11-14 11:56:24 +0300

    first commit

---
+++ odev/src/test/java/RestService/odev/OdevApplicationTests.java
@@ -0,0 +1,13 @@
+package RestService.odev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
